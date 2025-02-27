540b43b45c3c2f7d2cc79b04c39947ed5f221c31
Devesh Chaturvedi <dcdev001@gmail.com>
2025-02-27 16:04:39 +0530

    starting the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+function App() {
+    return <h1>Hi There!</h1>;
+}
+
+root.render(<App />);
