eae87e08fdb69caf4967e9f8736a9cd31f252410
Devesh Chaturvedi <dcdev001@gmail.com>
2025-03-05 14:19:09 +0530

    add some styles

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,17 @@
 function ProfileCard({title, handle, image}) {
     return (
-        <div>
-            <img src={image}/>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image}/>
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitile is-6">{handle}</p>
+                </div>
+            </div>
         </div>
     );
 }
