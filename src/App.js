59ca07ad080641ed089c9062b424f661027feecd
Devesh Chaturvedi <dcdev001@gmail.com>
2025-03-17 21:51:34 +0530

    add state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 function App() {
+    const [count, setCount] = useState(0);
+
     const handleClick = () => {
-        console.log("Hello");
+        setCount(count + 1);
     };
 
   return (
     <div>
         <button onClick={handleClick}>Add Animal</button>
+        <div>
+            Number of animals: {count}
+        </div>
     </div>
   );
 }
