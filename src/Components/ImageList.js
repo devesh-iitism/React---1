1a48111d977ba6124efb4820a43786940565e0e6
Devesh Chaturvedi <dcdev001@gmail.com>
2025-04-01 14:01:02 +0530

    add key

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -2,7 +2,7 @@ import ImageShow from "./ImageShow";
 
 function ImageList({ images }) {
     const renderedImages = images.map((image) => {
-        return <ImageShow image={image}/>;
+        return <ImageShow image={image} key={image.id}/>;
     });
 
     return <div>{renderedImages}</div>;
