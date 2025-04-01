e0fc23167cc5a2e12197312e9187b630b838534b
Devesh Chaturvedi <dcdev001@gmail.com>
2025-04-01 13:40:53 +0530

    communicate the list of images down

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import SearchBar from "./Components/SearchBar";
+import ImageList from "./Components/ImageList";
 import searchImages from "./api";
 
 function App() {
+    const [images, setImages] = useState([]);
+
     const handleSubmit = async (term) => {
         const result = await searchImages(term);
-        console.log(result);
+        setImages(result);
     };
 
-    return <div><SearchBar onSubmit={handleSubmit}/></div>;
+    return (
+        <div>
+            <SearchBar onSubmit={handleSubmit}/>
+            <ImageList images={images}/>
+        </div>
+    );
 }
 
 export default App;
