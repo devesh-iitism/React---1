3c491152557510cea583a00adbd4404f71ea74d9
Devesh Chaturvedi <dcdev001@gmail.com>
2025-04-02 15:32:54 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import { useState } from "react";
-import SearchBar from "./Components/SearchBar";
-import ImageList from "./Components/ImageList";
-import searchImages from "./api";
-
 function App() {
-    const [images, setImages] = useState([]);
-
-    const handleSubmit = async (term) => {
-        const result = await searchImages(term);
-        setImages(result);
-    };
-
     return (
-        <div>
-            <SearchBar onSubmit={handleSubmit}/>
-            <ImageList images={images}/>
+        <div> APP
         </div>
     );
 }
