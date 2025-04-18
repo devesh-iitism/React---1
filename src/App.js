ee1328d0958b1063880f65159ad2b922f64d4a18
Devesh Chaturvedi <dcdev001@gmail.com>
2025-04-18 20:11:46 +0530

    introduce useEffect and fetch records

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     const editBookById = (id, newTitle) => {
         const updatedBooks = books.map((book) => {
